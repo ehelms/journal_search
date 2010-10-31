199c66fddfd21e6431e8d8cd9805b32e48813e23
Eric D Helms <ericdhelms@gmail.com>
2010-10-30 20:08:24 -0400

    Changed citeseerx search engine to new search schema.

--- search/engines/citeseerx.py
+++ search/engines/citeseerx.py
@@ -10,39 +10,45 @@ class CiteSeerXSearch:
     def __init__(self):
         self.SEARCH_BASE_URL = "http://citeseerx.ist.psu.edu/search"
 
-    def search(self, terms, num_results=10):
+    def search(self, terms, count):
+        resp = self._get_response(terms, count)
+        titles = self._scrape(resp)
+        return titles
+        
+
+    def _get_response(self, terms, start):
+        params = urllib.urlencode({'q': "+".join(terms), 'submit' : 'Search',
+                                    'sort' : 'rlv', 
+                                    'start' : start })
+
+        url = self.SEARCH_BASE_URL + "?" + params
+        headers = { 'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.0; en-GB; rv:1.8.1.12) Gecko/20080201 Firefox/2.0.0.12' }
+        request = urllib2.Request(url, None, headers)
+        resp = urllib2.urlopen(request)
+        return resp
+        
+
+    def _scrape(self, response):
         titles = []
-        count = 0
-
-        while count < num_results:
-            params = urllib.urlencode({'q': "+".join(terms), 'submit' : 'Search',
-                                        'sort' : 'rlv', 
-                                        'start' : count })
-
-            url = self.SEARCH_BASE_URL + "?" + params
-            headers = { 'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.0; en-GB; rv:1.8.1.12) Gecko/20080201 Firefox/2.0.0.12' }
-            request = urllib2.Request(url, None, headers)
-            resp = urllib2.urlopen(request)
+        try:
+            html = response.read()
+            results = []
+            html = html.decode('ascii', 'ignore')
+                        
+            # Screen-scrape the result to obtain the publication information
+            BeautifulSoup.NESTABLE_TAGS['em'] = []
+            soup = BeautifulSoup(html)
             
-            try:
-                html = resp.read()
-                results = []
-                html = html.decode('ascii', 'ignore')
-                            
-                # Screen-scrape the result to obtain the publication information
-                BeautifulSoup.NESTABLE_TAGS['em'] = []
-                soup = BeautifulSoup(html)
-                
-                attrs = soup.findAll("em", { "class" : 'title'})
-                
-                for attr in attrs:
-                    title = ''.join(attr.findAll(text=True))
-                    titles.append(title)
+            attrs = soup.findAll("em", { "class" : 'title'})
+            
+            for attr in attrs:
+                title = ''.join(attr.findAll(text=True))
+                titles.append(title)
 
-            except urllib2.HTTPError:
-                print "ERROR: ",
-                print resp.status, resp.reason
-                return []
-            count = count + 10
+        except urllib2.HTTPError:
+            print "ERROR: ",
+            print resp.status, resp.reason
+            return []
                 
         return titles
+
